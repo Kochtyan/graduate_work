c55a6ca3975d10a702077157b5ca2abefbbe013c
Kochtyan <messka.sanfirov@inbox.ru>
2024-05-06 11:31:21 +0700

    search component

--- cinemach/frontend/src/app/components/header.js
+++ cinemach/frontend/src/app/components/header.js
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
-// import Button from "@mui/material/Button";
 import CustomButton from "./customButton";
+import SearchInput from "./searchInput";
 
 import { getPopularMovies } from "../api/tmdb";
 
@@ -28,6 +28,11 @@ function Header() {
             </li>
           </ul>
         </nav>
+        <SearchInput
+          placeholder="Поиск"
+          onChange={(event) => console.log(event.target.value)}
+          onClick={() => console.log("Search clicked")}
+        />
         <div className="header__login">
           <CustomButton title="Войти" style={{ width: "100px" }} />
         </div>
--- cinemach/frontend/src/app/components/searchInput.js
+++ cinemach/frontend/src/app/components/searchInput.js
@@ -1,14 +1,31 @@
+import React from "react";
 import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
 
-function SearchInput({ onSearch }) {
-  const handleSearchChange = (event) => {
-    const searchText = event.target.value;
-    onSearch(searchText);
+import "../css/searchInput.css";
+
+export default function SearchInput({ placeholder, onChange, onClick }) {
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      onClick();
+    }
   };
 
   return (
-    <TextField label="Поиск" variant="outlined" onChange={handleSearchChange} />
+    <TextField
+      placeholder={placeholder}
+      onChange={onChange}
+      onKeyDown={handleKeyDown}
+      className="searchInput__textField"
+      sx={{ borderRadius: 5, border: "1px solid #6C29A3", color: "#fff" }}
+      InputProps={{
+        endAdornment: (
+          <IconButton onClick={onClick} aria-label="search">
+            <SearchIcon sx={{ color: "#fff" }} />
+          </IconButton>
+        ),
+      }}
+    />
   );
 }
-
-export default SearchInput;
