1ca12060a96dcd941b1a809281c82cc293038cd6
Kochtyan <messka.sanfirov@inbox.ru>
2024-06-17 08:13:24 +0700

    actual db

--- cinemach/backend/DB/cinemachDB.sql
+++ cinemach/backend/DB/cinemachDB.sql
@@ -1,26 +1,134 @@
+CREATE DATABASE  IF NOT EXISTS `cinemach` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cinemach`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- File generated with SQLiteStudio v3.4.4 on Чт май 30 23:37:07 2024
+-- Host: localhost    Database: cinemach
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
 --
--- Text encoding used: System
+-- Table structure for table `movie_list`
 --
-PRAGMA foreign_keys = off;
-BEGIN TRANSACTION;
 
--- Table: MovieList
-CREATE TABLE IF NOT EXISTS MovieList ("List ID" INT REFERENCES UserMovieList (ID) NOT NULL, "Movie ID" INT REFERENCES Movies ("Movie ID") NOT NULL);
-INSERT INTO MovieList ("List ID", "Movie ID") VALUES (1, 342);
+DROP TABLE IF EXISTS `movie_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movie_list` (
+  `list_id` int NOT NULL,
+  `movie_id` int NOT NULL,
+  PRIMARY KEY (`list_id`),
+  KEY `Movie ID_idx` (`movie_id`),
+  CONSTRAINT `List ID` FOREIGN KEY (`list_id`) REFERENCES `user_movie_list` (`id`),
+  CONSTRAINT `Movie ID` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`movie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Table: Movies
-CREATE TABLE IF NOT EXISTS Movies (ID INTEGER NOT NULL, "Movie ID" INT NOT NULL PRIMARY KEY, Title VARCHAR NOT NULL, Year INT, Poster VARCHAR, Rating REAL);
-INSERT INTO Movies (ID, "Movie ID", Title, Year, Poster, Rating) VALUES (1, 342, 'Криминальное чтиво', 1999, 'https://image.openmoviedb.com/kinopoisk-images/1900788/87b5659d-a159-4224-9bff-d5a5d109a53b/orig', '8,5');
+--
+-- Dumping data for table `movie_list`
+--
 
--- Table: UserMovieList
-CREATE TABLE IF NOT EXISTS UserMovieList (ID INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE NOT NULL, "User ID" INT REFERENCES Users (ID) NOT NULL, "List Name" VARCHAR UNIQUE NOT NULL);
-INSERT INTO UserMovieList (ID, "User ID", "List Name") VALUES (1, 1, 'Fav');
+LOCK TABLES `movie_list` WRITE;
+/*!40000 ALTER TABLE `movie_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movie_list` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Table: Users
-CREATE TABLE IF NOT EXISTS Users (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, Name VARCHAR NOT NULL, Surname VARCHAR NOT NULL, Patronymic VARCHAR, Login VARCHAR NOT NULL UNIQUE, Password VARCHAR NOT NULL, Email VARCHAR UNIQUE NOT NULL);
-INSERT INTO Users (ID, Name, Surname, Patronymic, Login, Password, Email) VALUES (1, 'Kons', 'San', 'Vit', 'kochtyan', 'beats', 'koch@gmail.com');
+--
+-- Table structure for table `movies`
+--
 
-COMMIT TRANSACTION;
-PRAGMA foreign_keys = on;
+DROP TABLE IF EXISTS `movies`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movies` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `movie_id` int NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `poster` varchar(255) DEFAULT NULL,
+  `rating` float DEFAULT NULL,
+  PRIMARY KEY (`id`,`movie_id`),
+  KEY `fk_movie_id_idx` (`movie_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movies`
+--
+
+LOCK TABLES `movies` WRITE;
+/*!40000 ALTER TABLE `movies` DISABLE KEYS */;
+/*!40000 ALTER TABLE `movies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_movie_list`
+--
+
+DROP TABLE IF EXISTS `user_movie_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_movie_list` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `list_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_movie_list`
+--
+
+LOCK TABLES `user_movie_list` WRITE;
+/*!40000 ALTER TABLE `user_movie_list` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_movie_list` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `login` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idnew_table_UNIQUE` (`id`),
+  UNIQUE KEY `Email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'koch@gmail.com','koch','$2b$10$V7U67381xyLYeF8oA/EmNeqc6WvBflXBiOKK/KGFQjf9S3WSQlVlm'),(2,'yasanya@yandex.ru','yasanya','$2b$10$V7U67381xyLYeF8oA/EmNeqc6WvBflXBiOKK/KGFQjf9S3WSQlVlm'),(9,'mail@mail.com','testLogin','$2b$10$V7U67381xyLYeF8oA/EmNeqc6WvBflXBiOKK/KGFQjf9S3WSQlVlm'),(10,'mail@test.com','login','$2b$10$V7U67381xyLYeF8oA/EmNeqc6WvBflXBiOKK/KGFQjf9S3WSQlVlm'),(11,'mail123@test.com','login123','$2b$10$V7U67381xyLYeF8oA/EmNeqc6WvBflXBiOKK/KGFQjf9S3WSQlVlm');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-06-17  8:12:49
